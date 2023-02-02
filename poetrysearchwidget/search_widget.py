975495f03eb3b8dab314c1cb9140746003ae261d
Prica277 <prica277@hsd.k12.or.us>
2023-02-02 15:27:15 -0800

    updating widget

--- poetrysearchwidget/search_widget.py
+++ poetrysearchwidget/search_widget.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel
+from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QWidget, 
+                             QLabel, QHBoxLayout, QLineEdit, 
+                             QPushButton, QTextEdit)
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
 import sys
@@ -8,7 +10,7 @@ class Window(QWidget):
         self.resources = "poetrysearchwidget/resources/"
         super().__init__()
         self.resize(300, 300)
-        self.setWindowTitle("CodersLegacy")
+        self.setWindowTitle("The Poetry Widget")
         self.setWindowIcon(QIcon(self.resources + "icon.jpg"))
         self.UI()
 
@@ -16,9 +18,27 @@ class Window(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        label = QLabel("Hello World")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(label)
+        # Creating Widgets
+        title_label = QLabel("Poetry Search Widget")
+        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        description = "Search the PoetryDB database by"
+        description += " title, author, lines, linecount,"
+        description += " or poetrycount."
+        description_label = QLabel(description)
+
+        search_layout = QHBoxLayout()
+        self.search_field = QLineEdit()
+        search_button = QPushButton("Search")
+        search_layout.addWidget(self.search_field)
+        search_layout.addWidget(search_button)
+
+        results_text = QTextEdit("Results.")
+
+        layout.addWidget(title_label)
+        layout.addWidget(description_label)
+        layout.addLayout(search_layout)
+        layout.addWidget(results_text)
 
 def main():
     app = QApplication(sys.argv)
